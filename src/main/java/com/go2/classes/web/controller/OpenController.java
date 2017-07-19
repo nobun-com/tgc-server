b5fe359475b7ab143c30b1ab6fc0b68a4fdc866b
Shashank <shwashank12@gmail.com>
2017-07-19 12:35:54 +0530

    login register

--- src/main/java/com/go2/classes/web/controller/OpenController.java
+++ src/main/java/com/go2/classes/web/controller/OpenController.java
@@ -13,100 +13,105 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.go2.classes.business.service.PromoService;
 import com.go2.classes.business.service.StudentService;
 import com.go2.classes.business.service.TimeTableService;
 import com.go2.classes.common.BaseController;
 import com.go2.classes.models.Student;
 
-
 @Controller
 @RequestMapping("/")
-public class OpenController extends BaseController{
+public class OpenController extends BaseController {
 
-	@Resource
+    @Resource
     private StudentService studentService; // Injected by Spring
-	
-	@Resource
+
+    @Resource
     private TimeTableService timeTableService; // Injected by Spring
 
-	@RequestMapping(value="/openLogin", method=RequestMethod.POST)
-	public String openLogin(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		String redirectUrl = request.getParameter("redirectUrl");
-		Student student = studentService.findByEmail(email);
-		if(Objects.isNull(student)) {
-	        model.addAttribute("message", "user not found");
+    @Resource
+    private PromoService promoService; // Injected by Spring
+
+    @RequestMapping(value = "/openLogin", method = RequestMethod.POST)
+    public String openLogin(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
+	String email = request.getParameter("email");
+	String password = request.getParameter("password");
+	String redirectUrl = request.getParameter("redirectUrl");
+	Student student = studentService.findByEmail(email);
+	if (Objects.isNull(student)) {
+	    model.addAttribute("message", "user not found");
+	} else {
+	    if (password.equals(student.getPassword())) {
+		session.setAttribute("userName", student.getName());
+		session.setAttribute("userId", student.getId());
+		session.setAttribute("isLoggedIn", true);
+		session.setAttribute("userCartSize", timeTableService.getUserCartSize(student.getId()));
+		response.sendRedirect(redirectUrl);
+	    } else {
+		model.addAttribute("message", "login failed");
+	    }
+	}
+
+	model.addAttribute("popup", "LOGIN");
+	model.addAttribute("email", email);
+	model.addAttribute("password", password);
+	model.addAttribute("promos", promoService.findById(1l));
+
+	return "index";
+    }
+
+    @RequestMapping(value = "/openRegister", method = RequestMethod.POST)
+    public String openRegister(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
+
+	String name = request.getParameter("name");
+	String gender = request.getParameter("gender");
+	String email = request.getParameter("email");
+	String password = request.getParameter("password");
+	String confirmPassword = request.getParameter("confirmPassword");
+	String mobile = request.getParameter("mobile");
+	String refrralCode = request.getParameter("refrralCode");
+
+	Student student = studentService.findByEmail(email);
+
+	if (!Objects.isNull(student)) {
+	    model.addAttribute("message", "user alredy registerd with email " + email);
+	} else {
+	    if (password.equals(confirmPassword)) {
+		student = new Student();
+		student.setEmail(email);
+		student.setName(name);
+		student.setPassword(password);
+		student.setGender(gender);
+		student.setPhone(mobile);
+		student.setRefrralCode(refrralCode);
+		student = studentService.create(student);
+		if (Objects.isNull(student)) {
+		    model.addAttribute("message", "Unable to registred user");
 		} else {
-			if(password.equals(student.getPassword())) {
-		        session.setAttribute("userName", student.getName());
-		        session.setAttribute("userId", student.getId());
-		        session.setAttribute("isLoggedIn", true);
-		        session.setAttribute("userCartSize",timeTableService.getUserCartSize(student.getId()));
-		        response.sendRedirect(redirectUrl);
-			} else {
-		        model.addAttribute("message", "login failed");
-			}
+		    session.setAttribute("userName", student.getName());
+		    session.setAttribute("userId", student.getId());
+		    session.setAttribute("isLoggedIn", true);
+		    session.setAttribute("userCartSize", timeTableService.getUserCartSize(student.getId()));
+		    response.sendRedirect("/");
 		}
-		
-		model.addAttribute("popup", "LOGIN");
-		model.addAttribute("email", email);
-		model.addAttribute("password", password);
-		
-		return "index";
+	    } else {
+		model.addAttribute("message", "Password and confirm password doesn't match");
+	    }
 	}
+	model.addAttribute("popup", "SIGNUP");
+	model.addAttribute("name", name);
+	model.addAttribute("email", email);
+	model.addAttribute("password", password);
+	model.addAttribute("confirmPassword", confirmPassword);
+	model.addAttribute("mobile", mobile);
+	model.addAttribute("refrralCode", refrralCode);
+	model.addAttribute("promos", promoService.findById(1l));
+	return "index";
+    }
 
-	@RequestMapping(value="/openRegister", method=RequestMethod.POST)
-	public String openRegister(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException {
-
-		String name = request.getParameter("name");
-		String gender = request.getParameter("gender");
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		String confirmPassword = request.getParameter("confirmPassword");
-		String mobile = request.getParameter("mobile");
-		String refrralCode = request.getParameter("refrralCode");
-		
-		Student student = studentService.findByEmail(email);
-		
-		if(!Objects.isNull(student)) {
-	        model.addAttribute("message", "user alredy registerd with email " + email);
-		} else {
-			if(password.equals(confirmPassword)) {
-				student = new Student();
-				student.setEmail(email);
-				student.setName(name);
-				student.setPassword(password);
-				student.setGender(gender);
-				student.setPhone(mobile);
-				student.setRefrralCode(refrralCode);
-				student = studentService.create(student);
-				if(Objects.isNull(student)) {
-			        model.addAttribute("message", "Unable to registred user");
-				} else {
-					session.setAttribute("userName", student.getName());
-			        session.setAttribute("userId", student.getId());
-			        session.setAttribute("isLoggedIn", true);
-			        session.setAttribute("userCartSize",timeTableService.getUserCartSize(student.getId()));
-			        response.sendRedirect("/");
-				}
-			} else {
-				model.addAttribute("message", "Password and confirm password doesn't match");
-			}
-		}
-		model.addAttribute("popup", "SIGNUP");
-		model.addAttribute("name", name);
-		model.addAttribute("email", email);
-		model.addAttribute("password", password);
-		model.addAttribute("confirmPassword", confirmPassword);
-		model.addAttribute("mobile", mobile);
-		model.addAttribute("refrralCode", refrralCode);
-        return "index";
-	}
-
-	@RequestMapping(value="/logout")
-	public void addToCart(Model model,HttpSession session, HttpServletResponse response) throws IOException {
-		session.invalidate();
-		response.sendRedirect("/");
-	}
+    @RequestMapping(value = "/logout")
+    public void addToCart(Model model, HttpSession session, HttpServletResponse response) throws IOException {
+	session.invalidate();
+	response.sendRedirect("/");
+    }
 }
